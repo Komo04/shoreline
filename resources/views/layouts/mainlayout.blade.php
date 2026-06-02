efadd84bab0a2f3ed65c588b03184426ca386222
Komo04 <kusumawiranata7@gmail.com>
2026-06-03 01:58:32 +0800

    Fix bootstrap mixed content

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -128,7 +128,7 @@
 
     @include('components.web.footer')
 
-    <script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     {{-- SWEET ALERT GLOBAL --}}
     @include('components.swal')
